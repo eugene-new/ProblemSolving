9880289b0608cf82b3f33bc161545931e2db008c
ruwan9 <ruwan9@kakao.com>
2021-10-08 00:54:36 +0900

    백준 9095: 1,2,3더하기

--- [기초] 브루트포스/[완수] 1,2,3더하기.py
+++ [기초] 브루트포스/[완수] 1,2,3더하기.py
@@ -1,3 +1,19 @@
 import sys
 sys.stdin = open('input_files/1,2,3더하기.txt')
 
+
+def func(num):
+    if num == 1:  # 1
+        return 1
+    if num == 2:  # (1, 1),  (2)
+        return 2
+    if num == 3:  # (1, 1, 1), (1, 2), (1, 2), (3)
+        return 4
+    else:
+        return func(num-1) + func(num-2) + func(num-3)
+
+
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    print(func(n))
