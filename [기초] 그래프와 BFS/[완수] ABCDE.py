cc13cb393b8aeacc46d160dce86b064b840a2645
ruwan9 <ruwan9@kakao.com>
2021-10-14 01:11:50 +0900

    백준 13023: ABCDE

--- [기초] 그래프와 BFS/[완수] ABCDE.py
+++ [기초] 그래프와 BFS/[완수] ABCDE.py
@@ -1,35 +1,39 @@
 import sys
 sys.stdin = open('input_files/ABCDE.txt')
 
+# 깊이가 4 이상인 그래프 존재 유무!
+N, M = map(int, input().split())
+graph = [[] for _ in range(N)]
+visited = [False] * 2001
+ans = 0
 
-def dfs(graph, v, visited, depth):
+
+def dfs(v, depth):
     global ans
-    visited[v] = True
+    # 백트래킹
     if ans == 1:
         return
-
+    # 깊이가 4 이상이면 ans = 1
     if depth == 4:
         ans = 1
         return
 
     for i in graph[v]:
         if visited[i] == 0:
-            dfs(graph, i, visited, depth+1)
+            visited[i] = 1
+            dfs(i, depth + 1)
+            visited[i] = 0  # 초기화
 
-
-# 깊이가 4 이상인 그래프 존재 유무!
-N, M = map(int, input().split())
-graph = [[] for _ in range(N)]
-
+# graph 만들기
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
 
-
-ans = 0
-visited = [False] * (N+1)
+visited = [False] * N
 for i in range(N):
-    dfs(graph, i, visited, 1)
+    visited[i] = True
+    dfs(i, 0)
+    visited[i] = False  # 초기화 필요
 print(ans)
