5689d2b4c80b913f4211c263c4537d671647d340
ruwan9 <ruwan9@kakao.com>
2021-10-13 23:52:28 +0900

    백준 14501: 퇴사

--- [기초] 브루트포스/[완수] 퇴사.py
+++ [기초] 브루트포스/[완수] 퇴사.py
@@ -4,12 +4,12 @@ sys.stdin = open('input_files/퇴사.txt')
 N = int(input())
 data = list(list(map(int, input().split())) for _ in range(N))
 print(N, data)
-dp = [0] * (N+1)
+dp = [0] * (N+5)
 
-for i in range(N-1, -1, -1):
+for i in range(N-1, -1, -1):  # 맨 마지막부터 역으로
     if i + data[i][0] > N:
         dp[i] = dp[i+1]
     else:
-        dp[i] = max((data[i][1] + dp[i+data[i][0]]), dp[i+1])
-
+        dp[i] = max((data[i][1] + dp[i+data[i][0]]), dp[i+1])  # dp[i+1]과 비교
+print(dp)
 print(dp[0])
