9600d239466146e026f59a5e13145acebc08d9c4
ruwan9 <ruwan9@kakao.com>
2021-10-26 01:01:55 +0900

    feat: modify 이분그래프

--- [기초] 그래프와 BFS/[완수] 이분그래프.py
+++ [기초] 그래프와 BFS/[완수] 이분그래프.py
@@ -1,18 +1,5 @@
 import sys
 sys.stdin = open('input_files/이분그래프.txt')
-from collections import deque
-
-
-def bfs(start):
-    queue = deque([start])
-    visited[start] = True
-
-    while queue:
-        v = queue.popleft()
-        for i in graph[v]:
-            if visited[i] == False:
-                queue.append(i)
-                visited[i] = True
 
 
 K = int(input())
